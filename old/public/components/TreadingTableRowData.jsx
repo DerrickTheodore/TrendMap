b9f14bc928b64922b82e97ce4fa31ef389a1f80c
derricktheodoregit config --global user.email derrickxtheodore@gmail.comqlsqecslsesc <derrickxtheodore@gmail.com>
2018-06-16 20:01:54 -0400

    delete old files

--- old/public/components/TreadingTableRowData.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TreadingTableRowData = (props) => {
-  return (
-    <td>{props.data}</td>
-  )
-}
-
-export default TreadingTableRowData;
--- old/public/components/TweetsTableRowData.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TweetsTableRowData = (props) => {
-  return (
-    <td>{props.data}</td>
-  )
-}
-
-export default TweetsTableRowData;
